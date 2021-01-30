917c948ba69d031260ebaeb5ada795c79437a864
devidwolf <68775926+devidwolf@users.noreply.github.com>
2021-01-30 09:50:53 +0100

    init

---
+++ static/js/article.js
@@ -0,0 +1,37 @@
+$(function() {
+
+  /**
+   * built article contents navigation of headlines
+   * give each article headline refererencing link
+   */
+
+  let headlines = $('.entry-content h1, .entry-content h2, .entry-content h3, .entry-content h4, .entry-content h5, .entry-content h6');
+
+  // fill navigation
+  if (headlines.length !== 0) {
+    headlines.each(function(i, e) {
+      // console.log(i, e);
+      let lvl, link;
+
+      // extract headline number from tag:
+      // h1 -> 1 - 1 => 0; h2 -> 2 - 1 => 1; ...
+      lvl = Number($(e).prop('tagName').charAt(1)) - 1;
+      // console.log(lvl);
+
+      // nav items
+      link = $(`<li><a href='#${i}'>${$(this).text()}</a></li>`);
+      link.attr('data-nav-level', lvl);
+      link.appendTo('aside nav ul'); // add # to headings
+
+      // headlines
+      $(e).attr('id', i);
+      $(`<a href='#${i}'><i class='icon'>link</i></a>`).appendTo(e);
+
+    });
+  }
+  // no headings - no navigation, go hide it
+  else {
+    $('aside nav').hide();
+  }
+
+})
---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+$(function() {
+
+  /**
+   * highlight active nav item
+   */
+  let url = window.location.href;
+  // console.log(links);
+  $('header a, footer a').each(function(index, link) {
+    // console.log(link);
+    if (link.href === url) {
+      link.dataset.active = true;
+      link.removeAttribute('href');
+      return false; // https://stackoverflow.com/a/5515211/13765033
+    }
+  });
+
+  /**
+   * open close aside cards based on screen width
+   */
+   let breakpoint = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-md'));
+   // console.log(breakpoint);
+   $(window).on('load resize', function() {
+     // screen is big enough, expand all aside cards
+     if ($(window).width() > breakpoint) {
+       $('aside details').attr('open', true);
+     }
+     // screen to small, collapse all aside cards
+     else {
+       $('aside details').attr('open', false);
+     }
+   });
+
+});
